9f002d8b0849e5041b475cb49bced4747c21dbd4
Andrii Tsylia <andrii.tsylia@outlook.com>
2021-12-01 22:50:27 +0400

    Temporary commit (home -> work).

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Extensions.Polling;
-using System.Threading.Tasks;
-using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot
 {
@@ -25,7 +26,7 @@ namespace TelegramBot
             ReceiverOptions receiverOption = new ReceiverOptions { AllowedUpdates = { } };
 
             botClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOption, cts.Token);
-            
+
             Console.WriteLine($"Start listening for @{me.Username}");
             Console.ReadLine();
             cts.Cancel();
@@ -52,8 +53,8 @@ namespace TelegramBot
             {
                 return;
             }
-            
-            if (update.Message!.Type != MessageType.Text)
+
+            if (update.Message.Type != MessageType.Text)
             {
                 return;
             }
@@ -63,10 +64,91 @@ namespace TelegramBot
 
             Console.WriteLine($"Received a '{messageText}' message in chat {chatId}");
 
+            //Message sentMessage = await botClient.SendTextMessageAsync(
+            //    chatId: chatId,
+            //    text: "*You said*:\n" + messageText,
+            //    cancellationToken: cancellationToken);
+
+            //Message sentMessage = await botClient.SendTextMessageAsync(
+            //    chatId: chatId,
+            //    text: "Hi\\! _My name_ is *Andrii*\\! Press [https://www\\.google\\.com]",
+            //    parseMode: ParseMode.MarkdownV2,
+            //    disableNotification: true,
+            //    replyToMessageId: update.Message.MessageId,
+            //    replyMarkup: new InlineKeyboardMarkup(
+            //        InlineKeyboardButton.WithUrl(
+            //            "Press button",
+            //            "https://www.google.com")),
+            //    cancellationToken: cancellationToken);
+
+            //KeyboardButton[] row1 = { "10", "20", "30", "40", "50", "60", "70", "80" };
+            //ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+            //{
+            //    row1,
+            //    new KeyboardButton[] { "11", "22", "33", "44", "55", "66", "77", "88"},
+            //})
+            //{
+            //    ResizeKeyboard = true
+            //};
+
+            //Message sentMessage = await botClient.SendTextMessageAsync(
+            //    chatId: chatId,
+            //    text: "Choose a response",
+            //    replyMarkup: replyKeyboardMarkup,
+            //    cancellationToken: cancellationToken);
+
+            //ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+            //{
+            //    KeyboardButton.WithRequestLocation("Share Location"),
+            //    KeyboardButton.WithRequestContact("Share Contact"),
+            //});
+
+            //Message sentMessage = await botClient.SendTextMessageAsync(
+            //    chatId: chatId,
+            //    text: "Who or where are you?",
+            //    replyMarkup: replyKeyboardMarkup,
+            //    cancellationToken: cancellationToken);
+
+            //        InlineKeyboardMarkup inlineKeyboard = new(new[]
+            //{
+            //    // first row
+            //    new []
+            //    {
+            //        InlineKeyboardButton.WithCallbackData(text: "1.1", callbackData: "11"),
+            //        InlineKeyboardButton.WithCallbackData(text: "1.2", callbackData: "12"),
+            //    },
+            //    // second row
+            //    new []
+            //    {
+            //        InlineKeyboardButton.WithCallbackData(text: "2.1", callbackData: "21"),
+            //        InlineKeyboardButton.WithCallbackData(text: "2.2", callbackData: "22"),
+            //    },
+            //});
+
+            //            InlineKeyboardMarkup inlineKeyboard = new(new[]
+            //    {
+            //        InlineKeyboardButton.WithUrl(
+            //            text: "Link to the Repository",
+            //            url: "https://github.com/TelegramBots/Telegram.Bot"
+            //        )
+            //    }
+            //);
+
+            InlineKeyboardMarkup inlineKeyboard = new(new[]
+    {
+        InlineKeyboardButton.WithSwitchInlineQuery("switch_inline_query"),
+        InlineKeyboardButton.WithSwitchInlineQueryCurrentChat("switch_inline_query_current_chat"),
+    }
+);
+
             Message sentMessage = await botClient.SendTextMessageAsync(
                 chatId: chatId,
-                text: "You said:\n" + messageText,
+                text: "Removing keyboard",
+                replyMarkup: inlineKeyboard,
                 cancellationToken: cancellationToken);
+
+
+
         }
 
         public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
@@ -76,9 +158,9 @@ namespace TelegramBot
                 ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
                 _ => exception.ToString()
             };
-            
+
             Console.WriteLine(ErrorMessage);
-            
+
             return Task.CompletedTask;
         }
     }
